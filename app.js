4b64c9024b3759a175bd14c50cb676a173dd04a1
Mahrukh685 <mahrukhkhan685@gmail.com>
2024-01-18 02:07:02 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+
+let questions=[
+{   Id:1,
+    Question:"what does CSS???",
+    A:"Count Strike Source",
+    B:" Coorrective Style Sheet",
+    C:"Cascading Style Sheet",
+    D:"Computer Style Sheet",
+    Ans:"B",
+
+},
+{   Id:2,
+    Question:"what does CSS???",
+    A:"Count Strike Source",
+    B:" Coorrective Style Sheet",
+    C:"Cascading Style Sheet",
+    D:"Computer Style Sheet",
+    Ans:"A",
+},
+{   Id:3,
+    Question:"what does CSS???",
+    A:"Count Strike Source",
+    B:" Coorrective Style Sheet",
+    C:"Cascading Style Sheet",
+    D:"Computer Style Sheet",
+    Ans:"C",
+},
+]
+
