db0b0f4d650b0dbd6735195c3d5be2f520586657
Mahrukh685 <mahrukhkhan685@gmail.com>
2024-01-23 02:32:48 +0500

    option

--- app.js
+++ app.js
@@ -15,16 +15,27 @@ function getQuestion() {
     const option4 = document.createElement("h4")
     showindex.innerHTML = index + 1;
 
+    let div1=document.createElement("div");
+    let btn=document.createElement("button");
+     
+    
+
+
+
+
 
     question.textContent = questions[index].Question;
     option1.textContent = questions[index].A;
     option2.textContent = questions[index].B;
     option3.textContent = questions[index].C;
     option4.textContent = questions[index].D;
-
+   div1.appendChild(option1);
+   div1.appendChild(btn);
+   btn.textContent="a";
+   div1.classList.add("row")
 
     container.appendChild(question)
-    container.appendChild(option1)
+    container.appendChild(div1)
     container.appendChild(option2)
     container.appendChild(option3)
     container.appendChild(option4)
